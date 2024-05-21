3c635d96f23b4e963fbf38c3a118db48e654b4a1
yocci_ii <hiitoy0117@gmail.com>
2024-05-21 21:52:51 +0900

    commit

---
+++ libs/gtag.ts
@@ -0,0 +1,10 @@
+// npm i --save-dev @types/gtag.js
+export const GA_MEASUREMENT_ID = process.env.NEXT_PUBLIC_GA_ID || '';
+
+export const existsGaId = GA_MEASUREMENT_ID !== '';
+
+export const pageview = (path: string) => {
+  window.gtag('config', GA_MEASUREMENT_ID, {
+    page_path: path,
+  });
+};
---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+// $ npm install next-sitemap
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://*',
+  generateRobotsTxt: true,
+  sitemapSize: 7000,
+  outDir: './out',
+};
